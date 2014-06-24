57e4a27b6e908f46af29a3c1ef2817b73c914264
James Swineson <jamesswineson@gmail.com>
2014-06-24 09:51:48 +0800

    修正了底盘电机方向问题
    现在：手柄 3 通道前后对应底盘前后，手柄 1 通道左右对应底盘左右

--- extern.c
+++ extern.c
@@ -15,7 +15,7 @@ void main_process()
     DefineRobotMotors(2, 1, 3, 2, 7, 6, 0, 0, 0, 0);
     while(1) // main loop(joystick)
     {
-        Arcade4(1, 3, 1, 2, 3, 6, 7, 1, 1, 1, 1);
+        Arcade4(1, 3, 1, 3, 2, 7, 6, 0, 0, 0, 0);
         JoystickDigitalToMotorAndLimit(1, 5, 1, 127, 0, 2, -127, 0, 8);
         JoystickDigitalToMotorAndLimit(1, 5, 1, -127, 0, 2, 127, 0, 9);
         JoystickDigitalToMotorAndLimit(1, 6, 1, -127, 0, 2, 127, 0, 4);
